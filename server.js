7884cb703494e5da9e9406e9d1cd5a6637d5e13b
Logan <theapachewarrior@hotmail.com>
2022-09-12 08:39:38 -0400

    rollback

--- server.js
+++ server.js
@@ -1,31 +1,66 @@
-
 const express = require("express")
-const romanRouter = express()
+const app = express()
 require('dotenv').config();
 const methodOverride = require("method-override")
-romanRouter.use(methodOverride("_method"))
+app.use(methodOverride("_method"))
 const port = process.env.PORT;
-
-const mongoose = require('mongoose');
-const romanRouter = require("./controllers/roman.js");
+const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URI);
 const db = mongoose.connection
 db.on("connected", () => console.log("connected to MongoDB"));
 db.on("error", (err) => console.log("MongoDB error: " + err.message))
-
-romanRouter.use((req, res, next) => {
+app.use((req, res, next) => {
     console.log("I run for all routes")
     next()
 })
-romanRouter.use(express.urlencoded({ extended: true }))
-romanRouter.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+const roman = require("./models/romans.js");
 
-
-
-
-
-
-
+app.get("/Homepage", (req, res) => {
+    roman.find({},(error,allRoman) => {
+        res.render("index.ejs", {
+            roman: allRoman
+        })
+    })
+    });
+app.get("/Homepage/new", (req, res) => {
+    res.render("new.ejs")
+})
+app.post("/roman", (req,res) => {
+    if(req.body.conquering === "on"){
+        req.body.conqueriing = true
+    } else { req.body.conquering = false}
+    roman.create(req.body, (error, createdRoman) => {
+        res.send(createdRoman);
+    });
+    res.redirect("Homepage")
+});
+app.get("/Homepage/:id", (req,res) => {
+    roman.findById(req.params.id, (err, foundRoman) => {
+        res.render('show.ejs', {
+            roman: foundRoman,
+        })
+    })
+});
+app.delete("/homepage/:id", (req, res) => {
+    roman.findByIdAndDelete(req.params.id, (err, data) => {
+      res.redirect("/homepage")
+    })
+  })
+  app.get("/Homepage/:id/edit", (req, res) => {
+    roman.findById(req.params.id, (error, foundRoman) => {
+      res.render("edit.ejs", {
+        roman: foundRoman,
+      })
+    })
+  })
+  app.put("/Homepage/:id", (req, res) => {
+    if (req.body.completed === "on") {
+      req.body.completed = true
+    } else {
+      req.body.completed = false
+    }
   
     roman.findByIdAndUpdate(
       req.params.id,
@@ -37,4 +72,5 @@ romanRouter.use(express.static('public'))
         res.redirect(`/Homepage/${req.params.id}`)
       }
     )
+  })
 
